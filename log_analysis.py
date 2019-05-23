b6d33c6c4b3b9dcb5c41f0b663e75b44a7890838
caijiahe <caijiahe@kuaishou.com>
2019-05-23 10:57:10 +0800

    add comment and fix input/output stream not close

--- log_analysis.py
+++ log_analysis.py
@@ -12,30 +12,39 @@ class LogCountEntry:
 
 
 if __name__ == '__main__':
-    date = sys.argv[1]
+    log_start = sys.argv[1]
     input_file_name = sys.argv[2]
     output_file_name = sys.argv[3]
     key_idx = int(sys.argv[4])
 
-    old = time.clock()
-    print "count开始"
-    key = ""
-    count = {}
+    key = ""  # 统计key值，根据key_idx解析，目前只支持空格分隔
+    count = {}  # 统计map，key是统计key值，value是LogCountEntry
     input_file = open(input_file_name, 'r')
+
+    print "count开始"
+    old = time.clock()
     for row in input_file:
+        # 根据log_start解析本行key值，如果遇到pretty json这种带有换行符的日志，采用上次解析成功的key值统计
         all_match = re.split('\s+', row, key_idx + 1)
-        if all_match[0] == date:
+        if all_match[0] == log_start:
             key = all_match[key_idx]
+
+        # 逐行统计
         log_count_entry = count.get(key, LogCountEntry())
         log_count_entry.size += sys.getsizeof(row)
         log_count_entry.row += 1
         count[key] = log_count_entry
     print 'count结束，耗时%s' % (time.clock() - old)
+    input_file.close()
 
-    output_file = open(output_file_name, 'w+')
     old = time.clock()
     print "sort开始"
     sorted_count = sorted(count.items(), key=lambda x: x[1].size, reverse=True)
     print 'sort结束，耗时(%s)' % (time.clock() - old)
+
+    print "开始输出统计结果"
+    output_file = open(output_file_name, 'w+')
     for key, value in sorted_count:
         print >> output_file, '{key}: {size}字节, {row}行'.format(key=key, size=value.size, row=value.row)
+    output_file.close()
+    print "结束输出统计结果"
